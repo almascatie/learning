06adb416e29f2ac75b6d94675065d3f896468e8d
almascatie <31232574+almascatie@users.noreply.github.com>
2026-08-20 17:19:06 +0900

    Update manifest.js

--- stem/manifest.js
+++ stem/manifest.js
@@ -4,13 +4,13 @@ export const stemManifest = [
         grade: 2,
         title: "stem 1 gempa",
         description: "tugas gempa",
-        icon: "⏱️",
+        icon: "⏱️"
     },
     {
         id: "gempa",
         title: "Petualangan Menghadapi Gempa",
         description: "Belajar tentang gempa dan cara menyelamatkan diri.",
-        icon: "🌋",
+        icon: "🌋"
     }
 ];
 ];
