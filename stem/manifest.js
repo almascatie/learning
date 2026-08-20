0b78837001e58e07aae85d3334b192a81ccf7452
almascatie <31232574+almascatie@users.noreply.github.com>
2026-08-20 17:44:09 +0900

    Update manifest.js

--- stem/manifest.js
+++ stem/manifest.js
@@ -1,7 +1,7 @@
 export const stemManifest = [
     {
         id: "gempa",
-        title: "Petualang Menghadapi Gempa",
+        title: "Petualangan Menghadapi Gempa",
         description: "Belajar tentang gempa dan cara menyelamatkan diri.",
         icon: "🌋",
         file: "./gempa/gempa.js"
