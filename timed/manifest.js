6cb6d4db577a869d995f4d17af146b0838604fa8
almascatie <31232574+almascatie@users.noreply.github.com>
2026-08-20 16:56:46 +0900

    Update manifest.js

--- timed/manifest.js
+++ timed/manifest.js
@@ -1,1 +1,10 @@
-
+export const timedManifest = [
+    {
+        id: "timed-k2-001",
+        grade: 2,
+        title: "Tantangan Waktu 1",
+        description: "Tantangan dengan batas waktu",
+        icon: "⏱️",
+        file: "./kelas-2/timed-001.js"
+    }
+];
