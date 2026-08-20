a25545531a13ac481522db69c6fc09a555bc606c
almascatie <31232574+almascatie@users.noreply.github.com>
2026-08-20 14:37:12 +0900

    Update manifest.js

--- soal/manifest.js
+++ soal/manifest.js
@@ -19,12 +19,19 @@ export const quizManifest = [
         subject: "Matematika",
         title: "Matematika Kelas 2 — Paket 3",
         file: "./kelas-2/matematika-03.js"
-    }
+    },
     {
         id: "k2-matematika-04",
         grade: 2,
         subject: "Matematika",
         title: "Matematika Kelas 2 — Paket 4",
+        file: "./kelas-2/matematika-04.js"
+    },
+    {
+        id: "k2-bahasa-01",
+        grade: 2,
+        subject: "bahasa",
+        title: "Bahasa Kelas 2 — Paket 1",
         file: "./kelas-2/matematika-04.js"
     }
 ];
