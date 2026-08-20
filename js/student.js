0140de6391f92ff21aaa8cf13e8bac3d8b825e6f
almascatie <31232574+almascatie@users.noreply.github.com>
2026-08-20 14:57:16 +0900

    Update student.js

--- js/student.js
+++ js/student.js
@@ -1,6 +1,6 @@
 import { supabase } from "./supabase.js";
 import { db, saveSession, deleteSession } from "./storage.js";
-import { openQuizPackages } from "./quis.js";
+import { openQuizPackages } from "./quiz/quiz.js";
 
 const state = {
     selectedStudent: null,
